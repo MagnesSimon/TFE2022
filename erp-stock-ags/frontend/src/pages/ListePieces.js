a95bd3c3bd7f14a10f31319d44a7289746106ef0
MagnesSimon <simon-mag@hotmail.com>
2022-04-02 13:52:23 +0200

    Création des premières routes

---
+++ erp-stock-ags/frontend/src/pages/ListePieces.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ListePieces = () => {
+    return (
+        <div>
+            <h1>Futur tableau avec la liste des pièces</h1>
+        </div>
+    );
+};
+
+export default ListePieces;
